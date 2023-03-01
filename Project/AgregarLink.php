7772122f4601bbf20894680073bb66c353f168c1
JorgePootCarrillo <101065378+JorgePootCarrillo@users.noreply.github.com>
2023-03-01 16:46:47 -0600

    Validaciones para la inserción de Links
    Validaciones links

--- Project/AgregarLink.php
+++ Project/AgregarLink.php
@@ -3,8 +3,57 @@ include './Funciones.php';
 $nombreUrl = $_POST["NombreURL"];
 $cadenaUrl = $_POST["CadenaURL"];
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  if (empty($_POST['NombreURL'])) {
+  echo 'El campo de Nombre URL no puede estar vacío.';
+}else{
+    if (empty($_POST['CadenaURL'])) {
+  echo 'El campo de Cadena URL no puede estar vacío.';
+}else{
+    //Aqui iria si no son vacios
+    $mysqli = conectarSQLServer();
+    $sqlSentence = "SELECT * FROM links WHERE NombreLink = '".$_POST['NombreURL']."'";
+        $valida = $mysqli->query($sqlSentence);
+        if(mysqli_num_rows($valida) > 0){
+            echo 'El Nombre del Link (Identificador) ya existe en la base de datos.';
+        }else{
+           $sqlSentence = "SELECT * FROM links WHERE Link = '".$_POST['CadenaURL']."'";
+        $valida = $mysqli->query($sqlSentence);
+        if(mysqli_num_rows($valida) > 0){
+            echo 'La URl ya se encuentra registrada (http://...)';
+        }else{
+            //AQUÍ VA LA FUNCION DE AÑADIR LINKS A LA BD
+            echo'Puede Registrar';
+        $sqlSentence = "INSERT INTO `links` (`NombreLink`, `Link`) values ('$nombreUrl', '$cadenaUrl')";
+        $inserta = $mysqli->query($sqlSentence);
+        if($inserta){
+            $resultado = "Nueva URL ingresada";
+        }else{
+            $resulado = "No se pudo registrar la URL";
+        }
+        $mysqli->close();
+        }
+        }
+        
+       // $mysqli->close();
+    
+    
+    
+    //$query = "SELECT * FROM links WHERE NombreLinks = '".$_POST['NombreURL']."'";
+    //$resultado = mysqli_query($conexion, $query);
+    
+    
+    
 
-$resultado = "";
+}
+}
+
+
+}
+
+
+
+/*$resultado = "";
 
 $mysqli = conectarSQLServer();
 
@@ -22,5 +71,6 @@ if(!$mysqli){
     }
     
     echo $resultado;
-     header("location: IndexFront.php");
+     //header("location: IndexFront.php");
 
+*/
