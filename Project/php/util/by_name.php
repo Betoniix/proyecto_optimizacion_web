4637fb687f044700b953b77135e5171c3dc04262
Betoniix <miguelhumberto522@gmail.com>
2023-03-01 14:56:17 -0600

    order added

--- Project/php/util/by_name.php
+++ Project/php/util/by_name.php
@@ -1,5 +1,21 @@
 <?php 
     require_once('./mysql_handler.php');
     
-    
+    $type = $_POST["type"];
+
+    $db->begin_transaction();
+
+    try{
+        $stmp = $db->prepare("SELECT * FROM news ORDER BY ?");
+        $stmp->bind_param("s", $type);
+        $stmp->execute();
+        $db->commit();
+
+        $result = $stmp->get_result();
+        $data = $result->fetch_assoc();
+        echo var_dump($data);
+        
+    }catch(mysqli_sql_exception $exception){
+        $db->rollback();
+    }
 ?>
