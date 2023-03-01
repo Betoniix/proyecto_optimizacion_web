5d60cef11730b01f5a79d3e72ed423bbe4498f2f
betoniix <miguelhumberto522@gmail.com>
2023-02-28 23:14:19 -0600

    Queries added

--- Project/php/index.php
+++ Project/php/index.php
@@ -5,25 +5,41 @@ include_once('./util/mysql_handler.php');
 //https://rss.art19.com/apology-line
 //http://feeds.bbci.co.uk/news/world/rss.xml
 //https://feeds.simplecast.com/54nAGcIl
-
-    $url = "http://feeds.bbci.co.uk/news/world/rss.xml";
     
   
     $post_data = $_POST["links"];
-    
     $jsonUrls = json_decode($post_data);
+    $db->begin_transaction();
+    
 
-    $news = array();
 
-    foreach($jsonUrls->links as $link){       
+    try {
+        $news_query = $db->prepare("INSERT INTO `news`(`title`, `description`, `pubdate`, `link`, `id_host`) VALUES (?,?,?,?,?)");
+        $host_query = $db->prepare("INSERT INTO host (host_link) VALUES (?)");
+        $hostID_query = $db->prepare("SELECT id FROM host WHERE host_link = ?");
+
+    foreach($jsonUrls->links as $link){
+        
+        $host_query->bind_param("is", $link);
+        $host_query->execute();
+
+        $hostID_query->bind_param("is", $link);
+        $hostID_query->execute();
+        $result = $hostID_query->get_result();
+        $host_id = $result->fetch_assoc();
+
+        echo $host_id;
+
         $fetched = getNewsArray($link);
-        array_push($news, array($link => $fetched));
+
+        /* foreach($fetched as $new){
+            
+        } */
     }
     
-    $encoded = json_encode($news);
-    header('Content-Type: application/json');
-    header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-    header("Cache-Control: post-check=0, pre-check=0", false);
-    header("Pragma: no-cache");
-    echo $encoded;
+    $db->commit();
+    }catch(mysqli_sql_exception $exception){
+        $db->rollback();
+        echo $exception;
+    }
 ?>
--- Project/php/util/mysql_handler.php
+++ Project/php/util/mysql_handler.php
@@ -1,13 +1,29 @@
 <?php
-$host="localhost";
-$port=3305;
-$user="root";
-$password="";
-$dbname="opti_rss";
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-$con = new mysqli($host, $user, $password, $dbname, $port)
- or die ('Could not connect to the database server' . mysqli_connect_error());
+class MySqlHandler {
+    private $host="localhost";
+    private $port=3305;
+    private $user="root";
+    private $password="root";
+    private $dbname="opti_rss";
+    private $connection;
 
- 
-    
+    function __construct()
+    {
+        echo "hola";
+        $this->connection = new mysqli($this->host, $this->user, $this->password, $this->dbname, $this->port) or die ('Could not connect to the database server' . mysqli_connect_error());;
+        echo "hola";
+        $this->connection->autocommit(false);
+        echo "hola";
+    }
+
+    function connection(){
+        return $this->connection;
+    }
+}
+
+
+$handler = new MySqlHandler();
+$db = $handler->connection();
 ?>
