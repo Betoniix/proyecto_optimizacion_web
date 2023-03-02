201bfd30497b1fe773b28333bff92aa50e295736
Zenet1 <eduardo-orton2011@hotmail.com>
2023-03-02 01:00:30 -0600

    Update SaveNews.php and adding a date format js file

--- Project/php/SaveNews.php
+++ Project/php/SaveNews.php
@@ -1,33 +1,44 @@
 <?php
 include_once('./util/news_array.php');
 include_once('./util/mysql_handler.php');
-    
+
+
 $nombreUrl = $_POST['NombreURL'];
 $cadenaUrl = $_POST['CadenaURL'];
+
 $db->begin_transaction();
 
 try {
-    $host_exist = $db->prepare("SELECT IF(EXISTS(SELECT ID FROM links WHERE Link = ?), 1, 0)");
+    $host_exist = $db->prepare("SELECT IF(EXISTS(SELECT ID FROM links WHERE Link = ?), 1, 0) AS result");
     $news_query = $db->prepare("INSERT INTO news (title, description, pubdate, link, id_host, category) VALUES (?,?,?,?,?,?)");
     $host_query = $db->prepare("INSERT INTO links (NombreLink, Link) VALUES (?,?)");
     $hostID_query = $db->prepare("SELECT ID FROM links WHERE Link = ?");
     
-    $host_query->bind_param("ss", $nombreUrl, $cadenaUrl);
-    $host_query->execute();
+    $host_exist->bind_param("s", $cadenaUrl);
+    $host_exist->execute();
+
+    $result = $host_exist->get_result();
+    $exist_link = $result->fetch_assoc();
+
+    if(!$exist_link["result"]){
+        $host_query->bind_param("ss", $nombreUrl, $cadenaUrl);
+        $host_query->execute();
+        
+    }
 
     $hostID_query->bind_param("s", $cadenaUrl);
-    $hostID_query->execute();
+        $hostID_query->execute();
         
-    $result = $hostID_query->get_result();
-    $host_id = $result->fetch_assoc();
+        $result = $hostID_query->get_result();
+        $host_id = $result->fetch_assoc();
 
-    $fetched = getNewsArray($cadenaUrl);
-    print_r($host_id);
-    foreach($fetched as $new){
-        $news_query->bind_param("ssssis", $new["title"], $new["description"],$new["date"],$new["link"], $host_id["ID"], $new["category"]);
-        $news_query->execute();
-    }
+        $fetched = getNewsArray($cadenaUrl);
     
+        foreach($fetched as $new){    
+            $news_query->bind_param("ssssis", $new["title"], $new["description"],$new["date"],$new["link"], $host_id["ID"], $new["category"]);
+            $news_query->execute();
+        }
+
     $db->commit();
 
 }catch(mysqli_sql_exception $exception){
