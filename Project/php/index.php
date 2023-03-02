a653aafa6dc3d57658e006da0574181a77367661
Betoniix <miguelhumberto522@gmail.com>
2023-03-02 16:25:05 -0600

    a

--- Project/php/index.php
+++ Project/php/index.php
@@ -7,14 +7,23 @@ include_once('./util/mysql_handler.php');
     $db->begin_transaction();
 
     try {
-        $host_exist = $db->prepare("SELECT IF(EXISTS(SELECT id FROM host WHERE host_link = ?), 1, 0)");
+        $host_exist = $db->prepare("SELECT IF(EXISTS(SELECT id FROM host WHERE host_link = ?), 1, 0) as Result");
+        $new_exist = $db->prepare("SELECT IF(EXISTS(SELECT id FROM news WHERE title = ?), 1, 0) as result");
         $news_query = $db->prepare("INSERT INTO news(title,description,pubdate,link,id_host,category) VALUES (?,?,?,?,?,?)");
         $host_query = $db->prepare("INSERT INTO host (host_link) VALUES (?)");
         $hostID_query = $db->prepare("SELECT id FROM host WHERE host_link = ?");
 
     foreach($jsonUrls->links as $link){
-        $host_query->bind_param("s", $link);
-        $host_query->execute();
+        $host_exist->bind_param("s", $link);
+        $host_exist->execute();
+
+        $result = $host_exist->get_result();
+        $exist_link = $result->fetch_assoc();
+        
+        if(!$exist_link){
+            $host_query->bind_param("s", $link);
+            $host_query->execute();
+        }
 
         $hostID_query->bind_param("s", $link);
         $hostID_query->execute();
@@ -25,8 +34,16 @@ include_once('./util/mysql_handler.php');
         $fetched = getNewsArray($link);
         
         foreach($fetched as $new){
-            $news_query->bind_param("ssssis", $new["title"], $new["description"],$new["date"],$new["link"], $host_id["id"],$new["category"]);
-            $news_query->execute();
+            $new_exist->bind_param("s", $new["title"]);
+            $new_exist->execute();
+
+            $result = $new_exist->get_result();
+            $wasAdded = $result->fetch_assoc();
+
+            if(!$wasAdded["result"]){
+                $news_query->bind_param("ssssis", $new["title"], $new["description"],$new["date"],$new["link"], $host_id["id"],$new["category"]);
+                $news_query->execute();
+            }
         } 
     }
     
